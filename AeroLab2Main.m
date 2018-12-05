3cad3f4e8e6086fd974b766626b5c075d0a7a047
William Watkins <william.watkins@colorado.edu>
2018-12-05 13:14:38 -0700

    Update

--- AeroLab2Main.m
+++ AeroLab2Main.m
@@ -8,6 +8,12 @@ Raymie Fotherby
 
 %% Let's read in our data
 DaGuds = ReadMahInput();
+% this is for the sample data
+DaGuds = csvread('sample_set_2018.csv',1,0);
+%% Now let's do something with it
+%DaGuds = DaGuds'; Commented out because we're using the sample data which
+%is just a matrix, not a cell array. Will need to uncomment
 
-%% Now let's separate it
-DaGuds = DaGuds';
+for i = 1:length(DaGuds)
+    DaGuds(i) = TrailingEdge(DaGuds(i));
+end
